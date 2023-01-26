119d9cf3aa4ee9c19156191fff53fa806df1826b
JUNYI OU <49964599+Irvingouj@users.noreply.github.com>
2023-01-26 22:15:59 +0000

    test more fix

--- test/matrix_multiply_test.c
+++ test/matrix_multiply_test.c
@@ -1,8 +1,12 @@
-#include "matrix.h"
+#include "../include/matrix.h"
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
+
+
+    Matrix *m1 = New_Matrix_row_col(3, 3);
+    Matrix *m2 = New_Matrix_row_col(3, 3);
     
     
     return 0;
